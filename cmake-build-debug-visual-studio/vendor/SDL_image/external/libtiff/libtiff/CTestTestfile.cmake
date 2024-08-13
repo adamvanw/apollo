ca121a21fce9582c04d8a66b458f88faba49ca8f
adamvanw <80796919+adamvanw@users.noreply.github.com>
2024-08-13 00:35:02 -0500

    Removed IDE folders in repo

--- cmake-build-debug-visual-studio/vendor/SDL_image/external/libtiff/libtiff/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/adamv/repos/apollo/vendor/SDL_image/external/libtiff/libtiff
-# Build directory: C:/Users/adamv/repos/apollo/cmake-build-debug-visual-studio/vendor/SDL_image/external/libtiff/libtiff
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- cmake-build-debug-visual-studio/vendor/SDL_image/external/libtiff/port/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/adamv/repos/apollo/vendor/SDL_image/external/libtiff/port
-# Build directory: C:/Users/adamv/repos/apollo/cmake-build-debug-visual-studio/vendor/SDL_image/external/libtiff/port
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
